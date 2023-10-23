d76eb9c25033ab394d78bd0f3c250518a469af73
Landeri Srujan <srujanlanderi@gmail.com>
2023-10-23 17:30:08 +0530

    Designed Admin Dashboard

--- src/pages/AdminDashBoard.jsx
+++ src/pages/AdminDashBoard.jsx
@@ -6,6 +6,9 @@ import profilePhoto from '../Assets/profile.png'
 import chat from '../Assets/chat.png'
 import bell from '../Assets/bell.png'
 
+/* importing icons */
+import {IoAddCircle} from 'react-icons/io5'
+
 export default function AdminDashBoard() {
   return (
     <div className='admin'>
@@ -22,6 +25,45 @@ export default function AdminDashBoard() {
       </div>
 
       <div className="task-board">
+        <div class = "header">
+          <p>Task Board</p>
+          <IoAddCircle className='add-icon'/>
+        </div>
+
+        <div className= 'task'>
+          <span>Manager 1</span>
+          <span>3/5</span>
+        </div>
+
+        <div className= 'task'>
+          <span>Manager 2</span>
+          <span>2/5</span>
+        </div>
+
+        <div className= 'task'>
+          <span>Manager 3</span>
+          <span>1/5</span>
+        </div>
+
+        <div className= 'task'>
+          <span>Manager 4</span>
+          <span>0/5</span>
+        </div>
+
+        <div className= 'task'>
+          <span>Manager 5</span>
+          <span>0/5</span>
+        </div>
+
+        <div className='task'>
+          <span>Manager 6</span>
+          <span>0/5</span>
+        </div>
+
+        <div className='task'>
+          <span>Manager 7</span>
+          <span>0/5</span>
+        </div>
 
       </div>
 
